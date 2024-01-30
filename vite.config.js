43f23cc09bcca31cada7144d950d11d6a99cd395
fine20240123 <tomdu@hkaift.com>
2024-01-30 14:34:59 +0800

    tom_run_change_host

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'node:path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-  ,
-  server:{
-    proxy:{
-      '/api':{//获取路径中包含了/api的请求
-          target:'http://localhost:8080',//后台服务所在的源
-          changeOrigin:true,//修改源
-          rewrite:(path)=>path.replace(/^\/api/,'')///api替换为''
-      }
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        //获取路径中包含了/api的请求
+        target: "http://localhost:8082", //后台服务所在的源
+        changeOrigin: true, //修改源
+        rewrite: (path) => path.replace(/^\/api/, ""), ///api替换为''
+      },
+    },
+  },
+});
