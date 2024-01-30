c5229cd4f10055d31711253e2d8db8b648c866a4
White Tree <whitetree@Whitetreecodiinggalaxy.local>
2024-01-30 21:41:46 +0800

    tom_port

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         //获取路径中包含了/api的请求
-        target: "http://localhost:8082", //后台服务所在的源
+        target: "http://localhost:8080", //后台服务所在的源
         changeOrigin: true, //修改源
         rewrite: (path) => path.replace(/^\/api/, ""), ///api替换为''
       },
